259c298ed8e6fa001c4bdc3b918770ddb0b902e3
dgradziuk <damian.gradziuk@skni.umcs.pl>
2024-08-26 08:32:23 +0200

    fixed user id

--- backend/src/user/model.py
+++ backend/src/user/model.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String
 
 class User(Base):
     __tablename__ = "Users"
-    username = Column(String, primary_key=True, index=True)
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, primary_key=True)
     password = Column(String)
     email = Column(String, unique=True)

96f24e13fe1da811686c022376f0ab89a7c431e3
FoxedGuy <gradziuk.damian@o2.pl>
2025-03-30 16:34:03 +0200

    added initial admin creation

--- backend/setup.py
+++ backend/setup.py
@@ -1,5 +1,14 @@
 import logging
-from src.database.core import create_db, check_all_tables
+from src.database.core import create_db, check_all_tables, get_db
+from src.user.connector import get_user_by_username, create_new_user
+
+def create_admin_user():
+    with get_db() as db:
+        if not get_user_by_username(db, "admin"):
+            create_new_user(db, "admin", "admin_password","admin@admin")
+            logging.info("Admin user created")
+        else:
+            logging.info("Admin user already exists")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
@@ -12,5 +21,5 @@ if __name__ == "__main__":
     else:
         logging.info("All tables exists")
         logging.info("Nothing to do")
-
     logging.info("Creating admin user")
+    create_admin_user()

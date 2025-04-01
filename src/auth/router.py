25893fb72bc88d808d26f495dfa2f93d5c2e1eb1
FoxedGuy <gradziuk.damian@o2.pl>
2025-04-01 18:33:49 +0200

    Http only cookies for auth

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,28 +1,26 @@
+from datetime import timedelta
 from fastapi.security import OAuth2PasswordRequestForm
-
+from fastapi import Depends, Response, APIRouter
 from .exceptions import WrongPasswordException
 from .schemas import RegisterForm
 from ..database.core import get_db
-from fastapi import Depends, HTTPException
-from fastapi import APIRouter
-from ..auth.security import authenticate_user, create_access_token
+from ..auth.security import authenticate_user, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 from ..user.exceptions import UserAlreadyExistsException, UserNotFoundException, EmailAlreadyExistsException
-from datetime import timedelta
-from .security import ACCESS_TOKEN_EXPIRE_MINUTES
 from ..user.connector import create_new_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db=Depends(get_db)):
+async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends(), db=Depends(get_db)):
     try:
         user = authenticate_user(db,form_data.username, form_data.password)
     except UserNotFoundException:
         raise UserNotFoundException()
     except WrongPasswordException:
         raise WrongPasswordException()
-    token = create_access_token(data={"sub": user.username}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-    return {"access_token": token, "token_type": "bearer"}
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    response.set_cookie("access_token", httponly=True, value=f"Bearer {access_token}")
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/register")
 async def register_new_user(form_data: RegisterForm, db=Depends(get_db)):
--- src/auth/security.py
+++ src/auth/security.py
@@ -1,6 +1,6 @@
 from fastapi.exceptions import HTTPException
 from passlib.context import CryptContext
-from fastapi import Depends
+from fastapi import Depends, Cookie
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
@@ -11,8 +11,47 @@ from datetime import datetime, timedelta
 
 from ..user.exceptions import UserNotFoundException, UserNotAdminException
 from ..user.model import DBUser
+from fastapi.security import OAuth2
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi import Request
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi import HTTPException
+from fastapi import status
+from typing import Optional
+from typing import Dict
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+# Custom OAuth2PasswordBearer class that accepts access token from httpOnly Cookie
+class OAuth2PasswordBearerWithCookie(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[Dict[str, str]] = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.cookies.get("access_token")  #changed to accept access token from httpOnly Cookie
+        print("access_token is",authorization)
+
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
+
+
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/auth/token")
 
 SECRET_KEY = "your_super_secret_key"
 ALGORITHM = "HS256"
@@ -40,26 +79,26 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+def decode_access_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        user = get_user_by_username(db,username)
-        if username is None:
-            raise InvalidTokenException()
-        return user
+        return payload.get('sub')
     except JWTError:
         raise InvalidTokenException()
 
-async def get_current_admin(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+async def get_current_user(access_token: str = Depends(oauth2_scheme), db=Depends(get_db)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        user = get_user_by_username(db,username)
-        if username is None:
-            raise InvalidTokenException()
+        username = decode_access_token(access_token)
+        return get_user_by_username(db, username)
+    except InvalidTokenException:
+        raise InvalidTokenException()
+
+async def get_current_admin(access_token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+    try:
+        username = decode_access_token(access_token)
+        user = get_user_by_username(db, username)
         if user.privilege.name != "admin":
             raise UserNotAdminException()
         return user
-    except JWTError:
+    except InvalidTokenException:
         raise InvalidTokenException()
